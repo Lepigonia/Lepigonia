4d23b6ba6e9782dc14d3dc556a10119c55ec4e95
Lepigonia <schulermanuel811@gmail.com>
2026-08-13 17:14:12 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 50, fontFamily: 'sans-serif' }}>
+      <h1>Lepigonia läuft! 🚀</h1>
+      <p>Der Build funktioniert jetzt.</p>
+    </main>
+  );
+}
