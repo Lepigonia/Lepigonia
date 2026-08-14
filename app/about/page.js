42ce94881e31f22e63281e3f987f2c776e9c5ebf
Lepigonia <schulermanuel811@gmail.com>
2026-08-14 02:07:59 +0200

    Delete app directory

--- app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="prose">
-      <h1>About</h1>
-      <p>Hey, ich bin Manuel aka Lepigonia. Das hier ist mein kleines Web-Experiment.</p>
-    </div>
-  );
-}
