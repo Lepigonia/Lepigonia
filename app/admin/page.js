e9d7b678ac80bfd3988f32c90f55d181bf6d9479
Lepigonia <schulermanuel811@gmail.com>
2026-08-26 18:42:21 +0200

    refactor: extract admin content renderer

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,11 +1,9 @@
 "use client";
 
-import AdminAboutEditor from "../../components/admin/AdminAboutEditor";
-import AdminGalleryEditor from "../../components/admin/AdminGalleryEditor";
+import AdminContent from "../../components/admin/AdminContent";
 import AdminHeader from "../../components/admin/AdminHeader";
 import AdminLogin from "../../components/admin/AdminLogin";
 import AdminSidebar from "../../components/admin/AdminSidebar";
-import AdminStoryEditor from "../../components/admin/AdminStoryEditor";
 import { useAdminController } from "../../hooks/useAdminController";
 import { useAdminData } from "../../hooks/useAdminData";
 import "./admin.css";
@@ -34,37 +32,7 @@ export default function Admin() {
           onSelectCountry={controller.selectCountry}
           onAbout={controller.selectAbout}
         />
-        {editor.section === "about" && (
-          <AdminAboutEditor
-            about={data.about}
-            status={actions.status}
-            error={actions.error}
-            onChange={editor.updateAbout}
-            onSave={(event) => actions.saveAbout(event, data.about)}
-          />
-        )}
-        {editor.section === "stories" && (
-          <AdminStoryEditor
-            post={editor.post}
-            countries={data.countries}
-            status={actions.status}
-            error={actions.error}
-            onChange={editor.setPost}
-            onSave={(event) => actions.savePost(event, editor.post)}
-            onDelete={controller.deletePost}
-            onHeroUpload={actions.uploadHero}
-          />
-        )}
-        {editor.section === "gallery" && (
-          <AdminGalleryEditor
-            country={editor.currentCountry}
-            files={actions.galleryFiles}
-            status={actions.status}
-            error={actions.error}
-            onFiles={actions.uploadGallery}
-            onAction={actions.galleryAction}
-          />
-        )}
+        <AdminContent editor={editor} data={data} actions={actions} controller={controller} />
       </div>
     </main>
   );
