c877ab199ee44b06c97cfa45b30be4dfa360b01c
Lepigonia <schulermanuel811@gmail.com>
2026-08-26 19:01:02 +0200

    fix: add about route metadata

---
+++ app/about/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "About me",
+  description: "The person and story behind the Lepigonia travel journal.",
+};
+
+export default function AboutLayout({ children }) {
+  return children;
+}
