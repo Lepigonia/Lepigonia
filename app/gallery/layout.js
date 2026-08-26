e5b5bf024c31e2e4e44951e18914bb298d00cbdf
Lepigonia <schulermanuel811@gmail.com>
2026-08-26 19:00:57 +0200

    fix: add gallery route metadata

---
+++ app/gallery/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Gallery",
+  description: "Photographs from the journeys and stories of Lepigonia.",
+};
+
+export default function GalleryLayout({ children }) {
+  return children;
+}
